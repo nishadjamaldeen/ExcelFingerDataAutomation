c80cddfd23fa308e21146c538d6b444ad6ea0f49
eTreatMD <ryap@etreatmd.com>
2017-01-19 17:32:17 -0800

    Optimized Algorithm

--- src/ExcelFingerAutomation_SingleSubdirectory.py
+++ src/ExcelFingerAutomation_SingleSubdirectory.py
@@ -11,9 +11,6 @@
 
 
 
-
-
-
 import os
 from os import listdir
 import numpy as np
@@ -28,19 +25,45 @@ def get_sd(path):
     return filter(os.path.isdir, [os.path.join(path,f) for f in os.listdir(path)])
 
 def createExcelSheet(fileName, destinationPath):
+
+    measurementHeadings = ['Pip Joint Angle', 'Dip Joint Angle', 'Finger Length', 'Distal Midpoint Thickness',
+                            'Dip Joint Thickness', 'Middle Midpoint Thickness', 'Pip Joint Thickness',
+                            'Proximal Midpoint Thickness', 'Proximal Length', 'Middle Length', 'Distal Length']
+    deformityHeadings = ['Index Dip', 'Index Pip', 'Middle Dip', 'Middle Pip', 'Ring Dip', 'Ring Pip',
+                            'Little Dip', 'Little Pip']
+
     newXLfile = xlwt.Workbook(encoding="utf-8")
     sheet = newXLfile.add_sheet("Finger Measurement Data")
+    fingerCount = 4;
+    range_low = 2;
+    range_high = 13;
+
+    while fingerCount > 0:
+        for c in range(range_low, range_high):
+            sheet.write(1,c,measurementHeadings[c-range_low])
+        fingerCount-=1
+        range_low +=12
+        range_high +=12
+
+    
+
+    for c in range(50,58):
+        sheet.write(1,c,deformityHeadings[c-50])
+   
     compiledDataDestination = str(destinationPath) + '\\' + str(fileName) + '.xls'
     newXLfile.save(compiledDataDestination)
     return compiledDataDestination
+
 
 def rev(s):
     return ''.join(reversed(s))
     
 
+
 def excelOperations(measurementFilePath, deformityFilePath, subfolder, compiledDataDestination, row):
-    try:        
-        wb = xlrd.open_workbook(measurementFilePathilePath)
+    try:
+
+        wb = xlrd.open_workbook(measurementFilePath)
         sheetNamesArray = wb.sheet_names()
         workingSheet = wb.sheet_by_name(sheetNamesArray[0])
 
@@ -50,12 +73,13 @@ def excelOperations(measurementFilePath, deformityFilePath, subfolder, compiledD
         ring = [[workingSheet.cell_value(rr,cr) for rr in range(1,12)] for cr in range(3,4)]
         little = [[workingSheet.cell_value(rl, cl) for rl in range(1,12)] for cl in range(4,5)]
 		
-		wb_deformity = xlrd.open_workbook(deformityFilePath)
-		sheetNamesArray_deformity = wb.sheet_name()
-		deformityWorkingSheet = wb.sheet_by_name(sheetNamesArray_deformity[0])
+        wb_deformity = xlrd.open_workbook(deformityFilePath)
+        sheetNamesArray_deformity = wb.sheet_names()
+        deformityWorkingSheet = wb_deformity.sheet_by_name(sheetNamesArray_deformity[0])
 
-        deformityMatrix = [[deformityWorkingSheet.cell_value(ri,ci) for ri in range(0,1)] for ci in range(0,7)]
-
+        deformityMatrix = [[deformityWorkingSheet.cell_value(ri,ci) for ci in range(0,1)] for ri in range(0,8)]
+        print(deformityMatrix)
+        print(deformityMatrix[1][0])
         subfolderName = ''
         for c in reversed(subfolder):
             if (c == '\\'):
@@ -65,43 +89,39 @@ def excelOperations(measurementFilePath, deformityFilePath, subfolder, compiledD
         subfolderName = rev(subfolderName)
 
 
-
-
         savedFile = open_workbook(compiledDataDestination, "rb")
         savedFile_sheet = savedFile.sheet_by_index(0)
         writingFile = copy(savedFile)
         writingFile_sheet = writingFile.get_sheet(0)
-        writingFile_sheet.write(row,0, subfolderName)
+        writingFile_sheet.write(row+1,0, subfolderName)
 
 
-        writingFile_sheet.write_merge(0,0,2,13, 'Index', xlwt.easyxf("align: horiz center"))
-
+        
         for c in range(2,13):
             data = index[0][c-2]
             writingFile_sheet.write(row+1,c, data )
             
-
-        writingFile_sheet.write_merge(0,0,14,25, 'Middle', xlwt.easyxf("align:horiz center"))
         for d in range(14,25):
             data = middle[0][d-14]
             writingFile_sheet.write(row+1,d,data)
-
-
-        writingFile_sheet.write_merge(0,0,26,37, 'Ring' , xlwt.easyxf("align:horiz center"))
+        
         for e in range(26,37):
             data = ring[0][e-26]
             writingFile_sheet.write(row+1,e,data)
 
-        writingFile_sheet.write_merge(0,0,38,49, 'Little', xlwt.easyxf("align:horiz center"))
         for f in range(38, 49):
             data = little[0][f-38]
-            writingFile_sheet.write(row+1,f,data)
-			
-        writingFile_sheet.write_merge(0,0,50,58, 'Deformities', xlwt.easyxf("align:horiz center")
+            writingFile_sheet.write(row+1,f,data)	
+        
         for g in range(50,58):
-                data = deformityMatrix[0][g-50]
-                writingFile_sheet.write(row+1,g,data)
+            data = deformityMatrix[g-50][0]
+            writingFile_sheet.write(row+1,g,data)
 
+        writingFile_sheet.write_merge(0,0,2,13,'Index', xlwt.easyxf("align: horiz center"))
+        writingFile_sheet.write_merge(0,0,14,25,'Middle', xlwt.easyxf("align: horiz center"))
+        writingFile_sheet.write_merge(0,0,26,37,'Ring', xlwt.easyxf("align: horiz center"))
+        writingFile_sheet.write_merge(0,0,38,49,'Little', xlwt.easyxf("align: horiz center"))
+        writingFile_sheet.write_merge(0,0,50,58, 'Deformities', xlwt.easyxf("align:horiz center"))
 
         writingFile.save(compiledDataDestination)
         
@@ -129,7 +149,7 @@ def main():
             elif files.endswith('deformity.xls') or files.endswith('deformity.xlsx'):
                 deformity_filePath = str(subfolder) + '\\' + str(files)
 				    
-            excelOperations(measurement_filePath, deformity_filePath, subfolder, compiledDataDestination, row)
-                row = row+1
+        excelOperations(measurement_filePath, deformity_filePath, subfolder, compiledDataDestination, row)
+        row = row+1
 
 main()
